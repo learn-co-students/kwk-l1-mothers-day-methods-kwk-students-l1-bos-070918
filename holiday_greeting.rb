b93c70fd3f90f06923d495933d3cbcd040bbc4d5
Cece Zinny <cwzinny@gmail.com>
2018-07-10 17:41:23 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,11 @@
+def holiday_greeting(holiday="Mothers Day",name="Mom",signature="Your Favorite Child")
+  puts "Happy #{holiday}, #{name}! - From #{signature}"
+end
+
+holiday_greeting("Fourth of July","Beyonce","Jay-Z")
+holiday_greeting("First Day of Spring","Caroline","Cece")
+holiday_greeting
+
+
+# mothers_day("Mary")
+# mothers_day("Lynne")
