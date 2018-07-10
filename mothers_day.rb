b71bcd02c73f86ac78c3e23793f4eb902d6178ef
Cece Zinny <cwzinny@gmail.com>
2018-07-10 18:01:48 +0000

    Done.

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,12 +1,21 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
 def mothers_day(name="Mom")
-  puts "Happy Mother's Day, #{name}!"
+  "Happy Mother's Day, #{name}!"
+  # putting return before the message still works
 end
 
-mothers_day("Rachel")
-mothers_day("Susanna")
-mothers_day("Mary")
-mothers_day("Lynne")
+# def mothers_day(name="Mom")
+  # puts "Happy Mother's Day, #{name}!"     with the "puts", the message shows up in the terminal, but 2 errors show up when learn is typed in, so do not use the puts. this is using return instead and results in 0 errors so you can successfully learn submit
+# end
+
+
+# mothers_day("Rachel")
+# mothers_day("Susanna")
+# mothers_day("Mary")
+# mothers_day("Lynne")
+
 mothers_day
+mothers_day("Rachel")
+
 
