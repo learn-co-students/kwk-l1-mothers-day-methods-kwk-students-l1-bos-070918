c31d495909fa2842abdab0379d8242c9ceee48a3
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-10 18:03:40 +0000

    Done.

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -2,7 +2,9 @@ def mothers_day
   "Happy Mother's Day, I hope you have a great day!"
 end
 
-def mothers_day(name="Adebisi")
-  "Happy Mother's Day, #{name}! I hope you have a great day!"
+def mothers_day(name="Mom")
+  "Happy Mother's Day, #{name}!"
 end
 
+mothers_day("Beyonce")
+puts mothers_day
