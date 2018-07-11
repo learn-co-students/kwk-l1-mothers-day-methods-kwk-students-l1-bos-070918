59dd3389baa9849c3ab7a22dd573b2c78305904a
Celine Anyaegbunam <cea03@icloud.com>
2018-07-11 12:48:25 +0000

    Done.

---
+++ holiday_greeting.rb
@@ -0,0 +1,13 @@
+puts "Welcome User! What's your name?"
+name=gets.chomp.capitalize
+
+puts "What holiday is coming up?"
+holiday=gets.chomp.capitalize
+
+puts "Where would you like to go for your holiday vacation?"
+to_go=gets.chomp.capitalize 
+
+puts "Sounds fun! Where are you coming from?"
+from=gets.chomp.capitalize
+
+puts "Have a great #{holiday} in #{to_go}, #{name}!"
